754c6d1f4ecd367999c9b285a660d38579dd8c2f
Alec Jasik <alec_jasik@yahoo.com>
2020-08-04 20:17:03 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  ["hello", "goodbye"]
+  array.pop
+end
+
+def pop_with_args(array)
+  ["hello", "goodbye", "good_day"]
+  array.pop(2)
+end
+
+def using_shift(array)
+  ["hello"]
+  array.shift 
+end
+
+def shift_with_args(array)
+  ["hello", "goodbye"]
+  array.shift(2)
+end
